5c622b82be5a3d9665d975021b740b3acd3f2b05
gragas <wge3wtgdx2ret43eyhu5j13r3rt1t@users.noreply.github.com>
2024-11-14 23:52:00 +0100

    added save note function

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
-console.log('hello');
+const noteForm = document.querySelector('#noteForm');
+const titleNote = document.querySelector('#titleNote');
+const descriptionNote = document.querySelector('#descriptionNote');
+const statusText = document.querySelector('#statusText');
+
+let noteID = 0;
+let notesArray = [];
+
+console.log(notesArray);
+
+noteForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Check no empty spaces input
+  if (titleNote.value.trim() === '' || descriptionNote.value.trim() === '') {
+    statusText.textContent = 'Error: note cant be empty';
+    titleNote.value = '';
+    descriptionNote.value = '';
+  } else {
+    // Runs save note function
+    statusText.textContent = '';
+    saveNote(titleNote.value, descriptionNote.value);
+    titleNote.value = '';
+    descriptionNote.value = '';
+  }
+});
+
+function saveNote(title, description) {
+  const newNote = {
+    id: noteID++,
+    title: title,
+    content: description,
+    timestamp: new Date().toLocaleString(),
+  };
+
+  notesArray.push(newNote);
+  console.log(notesArray);
+
+  saveLocalStorage(newNote.title, newNote);
+}
+
+function saveLocalStorage(key, value) {
+  localStorage.setItem(JSON.stringify(key), JSON.stringify(value));
+}
+
+/* //.toLocaleString konverterar datumobjektet till en sträng i det lokala formatet
+console.log(new Date().toLocaleString()); */
